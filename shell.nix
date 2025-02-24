e5f42fc0d60a9a56e343974f95aed98661cf04dd
Alois Wohlschlager <alois1@gmx-topmail.de>
2025-02-24 11:16:52 +0100

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: Alois Wohlschlager <wohlschlager@math.lmu.de>
+# SPDX-License-Identifier: CC0-1.0
+let
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/3566ab7246670a43abd2ffa913cc62dad9cdf7d5.tar.gz";
+    sha256 = "1wwqlc4fp00swnas5p3ipzhmgz3iyk84apg2vfglr5r4mfm3m980";
+  }) { };
+in
+pkgs.mkShell {
+  strictDeps = true;
+  nativeBuildInputs = builtins.attrValues {
+    inherit (pkgs)
+      cargo
+      clippy
+      nil
+      nixfmt-rfc-style
+      reuse
+      rust-analyzer
+      rustc
+      rustfmt
+      singular
+      ;
+  };
+}
---
+++ src/main.rs
@@ -0,0 +1,220 @@
+// SPDX-FileCopyrightText: Alois Wohlschlager <wohlschlager@math.lmu.de>
+// SPDX-License-Identifier: EUPL-1.2
+
+use std::{cmp::Reverse, collections::HashSet};
+
+fn main() {
+    let n = std::env::args()
+        .nth(1)
+        .expect("missing argument")
+        .parse::<u32>()
+        .expect("invalid number");
+
+    let d = n * (n + 1) / 2;
+
+    println!(
+        "ring R = (0,v1,v2,v3,b(16..{d})),cs(1..{n}),ws(1..{n});
+poly b(1) = -1;
+poly b(2) = -v1;
+poly b(3) = -v1^2;
+poly b(4) = -2*v1^3-v2;
+poly b(5) = -4*v1^4-3*v1*v2;
+poly b(6) = -9*v1^5-9*v1^2*v2;
+poly b(7) = -21*v1^6-26*v1^3*v2-2*v2^2;
+poly b(8) = -51*v1^7-76*v1^4*v2-15*v1*v2^2-v3;
+poly b(9) = -127*v1^8-221*v1^5*v2-70*v1^2*v2^2-5*v1*v3;
+poly b(10) = -324*v1^9-644*v1^6*v2-281*v1^3*v2^2-7*v2^3-24*v1^2*v3;
+poly b(11) = -841*v1^10-1877*v1^7*v2-1044*v1^4*v2^2-77*v1*v2^3-95*v1^3*v3-6*v2*v3;
+poly b(12) = -2213*v1^11-5476*v1^8*v2-3708*v1^5*v2^2-505*v1^2*v2^3-339*v1^4*v3-63*v1*v2*v3;
+poly b(13) = -5889*v1^12-15997*v1^9*v2-12775*v1^6*v2^2-2618*v1^3*v2^3-1141*v1^5*v3-28*v2^4-399*v1^2*v2*v3;
+poly b(14) = -15821*v1^13-46800*v1^10*v2-43061*v1^7*v2^2-11931*v1^4*v2^3-3710*v1^6*v3-414*v1*v2^4-2001*v1^3*v2*v3-40*v2^2*v3;
+poly b(15) = -42851*v1^14-137104*v1^11*v2-142794*v1^8*v2^2-50190*v1^5*v2^3-11794*v1^7*v3-3482*v1^2*v2^4-8776*v1^4*v2*v3-556*v1*v2^2*v3-4*v3^2;
+poly cs(0) = 1;");
+    for i in n + 1..=d {
+        println!("poly cs({i}) = 0;");
+    }
+
+    // Step 1: compute expressions for the Chern classes of the dual
+    let mut known_monomial_symmetric_functions = HashSet::new();
+    println!("poly c(0) = 1;");
+    for i in 1..=n {
+        println!(
+            "poly c({i}) = {};",
+            (i..=d)
+                .flat_map(|w| partitions(w, i))
+                .map(|j| {
+                    j.iter()
+                        .map(|part| format!("b({part})"))
+                        .chain(std::iter::once(define_monomial_symmetric_function(
+                            n,
+                            &j,
+                            &mut known_monomial_symmetric_functions,
+                        )))
+                        .collect::<Vec<_>>()
+                        .join("*")
+                })
+                .collect::<Vec<_>>()
+                .join("+"),
+        );
+    }
+
+    // Step 2: compute the Chern subalgebra
+    println!("ideal I =");
+    for k in (1..=n).rev() {
+        println!(
+            "  {},",
+            ((2 * k).saturating_sub(n)..=u32::min(n, 2 * k))
+                .map(|i| format!("cs({i})*c({})", 2 * k - i))
+                .collect::<Vec<_>>()
+                .join("+"),
+        );
+    }
+    println!(
+        "  cs({n})^2;
+I = std(I);"
+    );
+
+    // Step 3: write the next stage, which will compute the cohomology ring
+    println!(
+        "print(\"ring R = (0,v1,v2,v3,b(16..{d}),d(16..{n})),z(1..{n}),ws(1..{n});
+option(redSB);
+poly d(1) = 2;
+poly d(2) = -v1;
+poly d(3) = 2*v1^2;
+poly d(4) = -8*v1^3-7*v2;
+poly d(5) = 26*v1^4+30*v1*v2;
+poly d(6) = -84*v1^5-111*v1^2*v2;
+poly d(7) = 300*v1^6+502*v1^3*v2+112*v2^2;
+poly d(8) = -1140*v1^7-2299*v1^4*v2-960*v1*v2^2-127*v3;
+poly d(9) = 4334*v1^8+9958*v1^5*v2+5414*v1^2*v2^2+766*v1*v3;
+poly d(10) = -16692*v1^9-43118*v1^6*v2-29579*v1^3*v2^2-2380*v2^3-3579*v1^2*v3;
+poly d(11) = 65744*v1^10+189976*v1^7*v2+161034*v1^4*v2^2+31012*v1*v2^3+17770*v1^3*v3+5616*v2*v3;
+poly d(12) = -262400*v1^11-837637*v1^8*v2-838452*v1^5*v2^2-240631*v1^2*v2^3-86487*v1^4*v3-55329*v1*v2*v3;
+poly d(13) = 1056540*v1^12+3685550*v1^9*v2+4232750*v1^6*v2^2+1600786*v1^3*v2^3+404198*v1^5*v3+58268*v2^4+363210*v1^2*v2*v3;
+poly d(14) = -4292816*v1^13-16254540*v1^10*v2-21110372*v1^7*v2^2-10071369*v1^4*v2^3-1864478*v1^6*v3-1022466*v1*v2^4-2193009*v1^3*v2*v3-212440*v2^2*v3;
+poly d(15) = 17587492*v1^14+71867828*v1^11*v2+104219628*v1^8*v2^2+60190566*v1^5*v2^3+8581604*v1^7*v3+10170952*v1^2*v2^4+12667346*v1^4*v2*v3+2972696*v1*v2^2*v3+65024*v3^2;\");");
+    for i in 1..=n {
+        println!(
+            "print(\"poly cs({i}) = (-1)^{i}*({});\");",
+            (0..=(n - i))
+                .map(|k| format!("d({})*z({})", k + 1, k + i))
+                .collect::<Vec<_>>()
+                .join("+"),
+        );
+    }
+    println!("print(\"ideal I =\");");
+    for i in 1..=n {
+        println!("printf(\"  cs({i})^2-(%s),\",reduce(cs({i})^2,I));");
+    }
+    println!(
+        "print(\"  0;
+I = std(I);\");
+quit;"
+    );
+}
+
+fn partitions(weight: u32, length: u32) -> Vec<Vec<u32>> {
+    if length == 1 {
+        vec![vec![weight]]
+    } else {
+        (1..=weight - length + 1)
+            .flat_map(|i| {
+                partitions(weight - i, length - 1)
+                    .into_iter()
+                    .filter_map(move |j| (j[0] <= i).then(|| std::iter::once(i).chain(j).collect()))
+            })
+            .collect()
+    }
+}
+
+fn define_monomial_symmetric_function(n: u32, j: &[u32], known: &mut HashSet<Vec<u32>>) -> String {
+    let name = format!(
+        "m({})",
+        j.iter()
+            .map(|part| format!("{part}"))
+            .collect::<Vec<_>>()
+            .join(",")
+    );
+    if !known.contains(j) {
+        if j.len() > n as usize {
+            println!("poly {name} = 0;");
+        } else if j[0] == 1 {
+            println!("poly {name} = cs({});", j.len());
+        } else {
+            let jr = j
+                .iter()
+                .filter_map(|part| (*part > 1).then_some(part - 1))
+                .collect::<Vec<_>>();
+            println!(
+                "poly {name} = reduce(cs({})*{}-({}),cs({n})^2);",
+                j.len(),
+                define_monomial_symmetric_function(n, &jr, known),
+                neighbours(&jr, j.len())
+                    .into_iter()
+                    .filter(|jn| jn != j)
+                    .map(|jn| format!(
+                        "{}*{}",
+                        neighbour_multiplicity(&jn, &jr),
+                        define_monomial_symmetric_function(n, &jn, known),
+                    ))
+                    .collect::<Vec<_>>()
+                    .join("+"),
+            );
+        }
+        known.insert(j.to_owned());
+    }
+    name
+}
+
+fn neighbours(jr: &[u32], k: usize) -> HashSet<Vec<u32>> {
+    if jr.is_empty() {
+        HashSet::from_iter([vec![1; k]])
+    } else {
+        neighbours(&jr[1..], k - 1)
+            .into_iter()
+            .map(|jn| std::iter::once(jr[0] + 1).chain(jn).collect())
+            .chain(neighbours(&jr[1..], k).into_iter().map(|jn| {
+                let mut result = std::iter::once(jr[0]).chain(jn).collect::<Vec<_>>();
+                result.sort_by_key(|part| Reverse(*part));
+                result
+            }))
+            .collect()
+    }
+}
+
+fn neighbour_multiplicity(jn: &[u32], jr: &[u32]) -> u32 {
+    do_neighbour_multiplicity(
+        jn,
+        &jr.iter()
+            .copied()
+            .chain(std::iter::repeat_n(0, jn.len() - jr.len()))
+            .collect::<Vec<_>>(),
+    )
+}
+
+fn do_neighbour_multiplicity(jn: &[u32], jr: &[u32]) -> u32 {
+    if jn.is_empty() {
+        1
+    } else {
+        assert!(jn[0] == jr[0] || jn[0] == jr[0] + 1);
+        let lead_n = jn.iter().take_while(|part| **part == jn[0]).count();
+        let lead_r = jr.iter().take_while(|part| **part == jr[0]).count();
+        if jn[0] > jr[0] {
+            assert!(lead_n <= lead_r);
+            neighbour_multiplicity(&jn[lead_n..], &jr[lead_n..])
+        } else {
+            assert!(lead_n >= lead_r);
+            assert!((lead_r..lead_n).all(|i| jn[i] == jr[i] + 1));
+            binom(lead_n as u32, lead_r as u32)
+                * neighbour_multiplicity(&jn[lead_n..], &jr[lead_n..])
+        }
+    }
+}
+
+fn binom(n: u32, k: u32) -> u32 {
+    if k == 0 {
+        1
+    } else {
+        n * binom(n - 1, k - 1) / k
+    }
+}
