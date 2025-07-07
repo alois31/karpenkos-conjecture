164e88cb2f357ddd171314149649d325f9761c18
Alois Wohlschlager <wohlschlager@math.lmu.de>
2025-07-07 15:55:54 +0200

    Increase the precision of approximate computations
    Going to degree 4 allows us to reproduce the counterexample for Spin_15,
    confirming the not so trustworthy computation by Singular. Also maybe it will
    allow finding additional counterexamples. However it does increase the runtime
    significantly.

--- approximate/src/main.rs
+++ approximate/src/main.rs
@@ -10,7 +10,7 @@ use std::{
 };
 
 type Exponent = u8;
-type Coefficient = i16;
+type Coefficient = i32;
 type ZMask = u32;
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
@@ -111,7 +111,7 @@ fn multiply_monomial_monomial(
     if m2.zs == 0 {
         let v1 = m1.v1 + m2.v1;
         let v2 = m1.v2 + m2.v2;
-        if v1 + 3 * v2 > 3 {
+        if v1 + 3 * v2 > 4 {
             Element::default()
         } else {
             Element::from_iter(std::iter::once((BasisElement { v1, v2, zs: m1.zs }, 1)))
@@ -184,6 +184,12 @@ fn rational_element(
             term(-1, 3, 0, &[1, 3], n),
             term(4, 0, 1, &[4], n),
             term(-1, 0, 1, &[1, 3], n),
+            term(-4, 4, 0, &[5], n),
+            term(-6, 4, 0, &[1, 4], n),
+            term(-1, 4, 0, &[2, 3], n),
+            term(-6, 1, 1, &[5], n),
+            term(-8, 1, 1, &[1, 4], n),
+            term(1, 1, 1, &[2, 3], n),
         ]
         .into_iter()
         .flatten(),
@@ -196,6 +202,8 @@ fn rational_element(
                 term(2, 2, 0, &[i + 2], n),
                 term(-8, 3, 0, &[i + 3], n),
                 term(-7, 0, 1, &[i + 3], n),
+                term(26, 4, 0, &[i + 4], n),
+                term(30, 1, 1, &[i + 4], n),
             ]
             .into_iter()
             .flatten()
@@ -246,6 +254,11 @@ fn main() {
             term(-1, 0, 1, &[5], n),
             term(4, 0, 1, &[1, 4], n),
             term(1, 0, 1, &[2, 3], n),
+            term(16, 4, 0, &[6], n),
+            term(-26, 4, 0, &[1, 5], n),
+            term(17, 1, 1, &[6], n),
+            term(-32, 1, 1, &[1, 5], n),
+            term(2, 1, 1, &[2, 4], n),
         ]
         .into_iter()
         .flatten()
@@ -253,7 +266,38 @@ fn main() {
         .filter(|(m, c)| *c != 0 && m.zs == m.zs & ((1 << n) - 1))
         .collect(),
     ))
-    .chain((2..=n).map(|k| {
+    .chain(std::iter::once((
+        2,
+        [
+            term(1, 0, 0, &[4], n),
+            term(-2, 0, 0, &[1, 3], n),
+            term(2, 1, 0, &[5], n),
+            term(-2, 1, 0, &[1, 4], n),
+            term(1, 1, 0, &[2, 3], n),
+            term(2, 2, 0, &[6], n),
+            term(-1, 2, 0, &[2, 4], n),
+            term(6, 3, 0, &[1, 6], n),
+            term(-8, 3, 0, &[2, 5], n),
+            term(7, 3, 0, &[3, 4], n),
+            term(2, 0, 1, &[1, 6], n),
+            term(-4, 0, 1, &[2, 5], n),
+            term(6, 0, 1, &[3, 4], n),
+            term(7, 4, 0, &[8], n),
+            term(-8, 4, 0, &[1, 7], n),
+            term(7, 4, 0, &[2, 6], n),
+            term(-12, 4, 0, &[3, 5], n),
+            term(13, 1, 1, &[8], n),
+            term(-20, 1, 1, &[1, 7], n),
+            term(17, 1, 1, &[2, 6], n),
+            term(-18, 1, 1, &[3, 5], n),
+        ]
+        .into_iter()
+        .flatten()
+        .map(|(m, c)| (m, (-c) & ((1 << t) - 1)))
+        .filter(|(m, c)| *c != 0 && m.zs == m.zs & ((1 << n) - 1))
+        .collect(),
+    )))
+    .chain((3..=n).map(|k| {
         let k_ = Coefficient::from(k);
         (
             k,
@@ -345,6 +389,80 @@ fn main() {
                         n,
                     )
                 }))
+                .chain(term(
+                    (2 * k_ * k_ * k_ * k_ + 12 * k_ * k_ * k_ + 46 * k_ * k_ - 108 * k_ - 1008)
+                        / 48,
+                    4,
+                    0,
+                    &[2 * k + 4],
+                    n,
+                ))
+                .chain(term(
+                    -(k_ * k_ * k_ * k_ + 2 * k_ * k_ * k_ + 11 * k_ * k_ - 86 * k_ - 432) / 12,
+                    4,
+                    0,
+                    &[1, 2 * k + 3],
+                    n,
+                ))
+                .chain(term(
+                    (k_ * k_ * k_ * k_ + 11 * k_ * k_ - 108 * k_ - 384) / 12,
+                    4,
+                    0,
+                    &[2, 2 * k + 2],
+                    n,
+                ))
+                .chain(term(
+                    -(k_ * k_ * k_ * k_ - 4 * k_ * k_ * k_ + 29 * k_ * k_ - 146 * k_ - 288) / 12,
+                    4,
+                    0,
+                    &[3, 2 * k + 1],
+                    n,
+                ))
+                .chain(term(
+                    (k_ * k_ * k_ * k_ - 8 * k_ * k_ * k_ + 47 * k_ * k_ - 124 * k_ - 204) / 12,
+                    4,
+                    0,
+                    &[4, 2 * k],
+                    n,
+                ))
+                .chain((5..=k + 1).flat_map(|i| {
+                    let i_ = Coefficient::from(i);
+                    term(
+                        Coefficient::pow(-1, i.into())
+                            * ((k_ - i_) * (k_ - i_) * (k_ - i_) * (k_ - i_)
+                                + 8 * (k_ - i_) * (k_ - i_) * (k_ - i_)
+                                + 47 * (k_ - i_) * (k_ - i_)
+                                + 124 * (k_ - i_)
+                                + 108)
+                            / 12,
+                        4,
+                        0,
+                        &[i, 2 * k + 4 - i],
+                        n,
+                    )
+                }))
+                .chain(term(k_ * k_ - 21, 1, 1, &[2 * k + 4], n))
+                .chain(term(-(2 * k_ * k_ - 4 * k_ - 40), 1, 1, &[1, 2 * k + 3], n))
+                .chain(term(2 * k_ * k_ - 7 * k_ - 37, 1, 1, &[2, 2 * k + 2], n))
+                .chain(term(
+                    -(2 * k_ * k_ - 11 * k_ - 28),
+                    1,
+                    1,
+                    &[3, 2 * k + 1],
+                    n,
+                ))
+                .chain(term(2 * k_ * k_ - 8 * k_ - 22, 1, 1, &[4, 2 * k], n))
+                .chain((5..=k + 1).flat_map(|i| {
+                    let i_ = Coefficient::from(i);
+                    term(
+                        Coefficient::pow(-1, i.into())
+                            * (2 * (k_ - i_) * (k_ - i_) + 8 * (k_ - i_) + 8),
+                        1,
+                        1,
+                        &[i, 2 * k + 4 - i],
+                        n,
+                    )
+                }))
                 .map(|(m, c)| {
                     (
                         m,
@@ -355,7 +473,7 @@ fn main() {
                 .collect(),
         )
     }))
-    .collect::<HashMap<_, _>>();
+    .collect();
 
     let mut cache = Default::default();
     let d = u8::try_from(u16::from(n) * u16::from(n + 1) / 2).unwrap();
@@ -364,7 +482,7 @@ fn main() {
             .filter(|i| mask & (1 << (i - 2)) != 0)
             .collect::<Vec<_>>();
         for d1 in 0..=d - cs.iter().sum::<u8>() {
-            if d1 + cs.iter().sum::<u8>() >= d - 3 {
+            if d1 + cs.iter().sum::<u8>() >= d.saturating_sub(4) {
                 println!(
                     "u^{{{d1}}}c_{{{}}}^*≡{}",
                     cs.iter()
