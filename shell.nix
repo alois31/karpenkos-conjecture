94861c59b0c5a16f1ed0a960b0c2d85786057f4f
Alois Wohlschlager <alois1@gmx-topmail.de>
2025-07-16 10:37:59 +0200

    Fix computation errors in the exact computations
    There has been an off-by-one bug [1] in Singular leading to incorrect results
    in the supposedly exact computations. Apply the relevant patches to restore
    correctness.
    
    [1] https://github.com/Singular/Singular/issues/1285

--- shell.nix
+++ shell.nix
@@ -1,10 +1,35 @@
 # SPDX-FileCopyrightText: Alois Wohlschlager <wohlschlager@math.lmu.de>
 # SPDX-License-Identifier: CC0-1.0
 let
-  pkgs = import (builtins.fetchTarball {
-    url = "https://releases.nixos.org/nixos/unstable/nixos-25.11pre805967.62b852f6c674/nixexprs.tar.xz";
-    sha256 = "sha256-0VFAX85s2QcFpcNVy7J4yn7j8Cn2IZADd1wsbT+1TYs=";
-  }) { };
+  pkgs =
+    import
+      (builtins.fetchTarball {
+        url = "https://releases.nixos.org/nixos/unstable/nixos-25.11pre805967.62b852f6c674/nixexprs.tar.xz";
+        sha256 = "sha256-0VFAX85s2QcFpcNVy7J4yn7j8Cn2IZADd1wsbT+1TYs=";
+      })
+      {
+        overlays = [
+          (final: prev: {
+            nix = final.lixPackageSets.latest.lix;
+            singular = prev.singular.overrideAttrs (old: {
+              patches = old.patches or [ ] ++ [
+                (final.fetchpatch {
+                  url = "https://github.com/Singular/Singular/commit/926ff5410741911f311f3cef9ddfb4fd7c789bc3.diff";
+                  hash = "sha256-bdfW/H2g4Mlog3JPz5sDhuniRwsbbAl1haKupVBgJVU=";
+                })
+                (final.fetchpatch {
+                  url = "https://github.com/Singular/Singular/commit/0e16e23693fa02fbdc5e5208489b18a15fe30318.diff";
+                  hash = "sha256-4mGqAT+3aw+tC+Cp9yUoP6RLzC2tPBwLa5a74qbsGig=";
+                })
+                (final.fetchpatch {
+                  url = "https://github.com/Singular/Singular/commit/ac5186c27ddaa91a04cdd7a294fb91bdfc0ba281.diff";
+                  hash = "sha256-O9CwclLhPWZx6zVKOxyF210IJUbD9vpUPmDcngVjszQ=";
+                })
+              ];
+            });
+          })
+        ];
+      };
 in
 pkgs.mkShell {
   strictDeps = true;
